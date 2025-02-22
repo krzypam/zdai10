9ac0f8025f2721365ce39658f7466c56b7d72b21
test <test@test.pl>
2025-02-22 08:57:06 +0000

    Add validate_model

--- train_model.py
+++ train_model.py
@@ -1,7 +1,7 @@
 from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import accuracy_score
+from joblib import dump
 
 X, y = load_iris(return_X_y=True)
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
@@ -9,6 +9,4 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_
 model = LogisticRegression(max_iter=1000)
 model.fit(X_train, y_train)
 
-y_pred = model.predict(X_test)
-accuracy = accuracy_score(y_test, y_pred)
-print(f"Accuracy: {accuracy}")
+dump(model, 'model.pkl')
---
+++ validate_model.py
@@ -0,0 +1,15 @@
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from joblib import load
+from sklearn.metrics import accuracy_score
+
+X, y = load_iris(return_X_y=True)
+_, X_test, _, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+model = load('model.pkl')
+
+y_pred = model.predict(X_test)
+accuracy = accuracy_score(y_test, y_pred)
+threshold = 0.8
+assert accuracy > threshold, "Accuracy is below given threshold"
+print(f'Accuracy: {accuracy}')
